f30585b6899682d0d04eedf4993fed712d25c764
ameerthehacker <ameerjhanprof@gmail.com>
2018-10-25 13:49:23 +0530

    Add basic cli functionalities :hammer:

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Hello world from cargo!');
+#!/usr/bin/env node
+const cli = require('commander');
+const version = require('./package.json').version;
+
+cli
+  .version(version, '-v, --version');  
+
+cli
+  .command('update')
+  .description('Update local cargoes')
+  .action(() => {
+    const update = require('./cli/update');
+
+    update();
+  });
+ 
+cli.parse(process.argv);
---
+++ cli/update.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk');
+
+module.exports = () => {
+  console.log(chalk.green('Updating cargoes...'));
+}
